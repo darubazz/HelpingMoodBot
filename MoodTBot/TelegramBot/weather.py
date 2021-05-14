fef81ef0b2a75a1e3be1e8e8d7f590ddce43cbf5
darubazz <darubazz@users.noreply.github.com>
2021-05-14 18:30:51 +0700

    Добавила метод получения информации о погоде
    Я получаю информацию о температуре, скорости ветра, облаках и влажности

--- MoodTBot/TelegramBot/weather.py
+++ MoodTBot/TelegramBot/weather.py
@@ -1,4 +1,18 @@
 import pyowm
 from config import OWM_TOKEN
 
+<<<<<<< Updated upstream
 owm = pyowm.OWM(OWM_TOKEN)
+=======
+owm = pyowm.OWM(OWM_TOKEN)
+
+def get_forecast(place):
+	observation = owm.weather_at_place(place)
+	weather = observation.get_weather()
+	temperature = weather.get_temperature('celsius')["temp"]
+	wind = weather.get_wind()['speed']
+	clouds = weather.get_clouds()
+	humidity = weather.get_humidity()
+	forecast = f"🏙 In {place} is currently {weather.get_detailed_status()} \n🌡️ {temperature} °C \n💨 {wind} m/s \n🌫️ {clouds} % \n💦 {humidity} %"
+	return forecast
+>>>>>>> Stashed changes
