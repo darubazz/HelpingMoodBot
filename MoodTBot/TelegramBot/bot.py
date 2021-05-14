2e95550b1f220408c047ee2b0c229115ca969a53
darubazz <darubazz@users.noreply.github.com>
2021-05-14 17:51:46 +0700

    Добавила команду /start, кнопки
    Добавила описание к сообщению. Кнопки пока не кликабельны, кроме /start. Добавила метод polling

--- MoodTBot/TelegramBot/bot.py
+++ MoodTBot/TelegramBot/bot.py
@@ -7,3 +7,18 @@ from utils.weather import get_forecast
 
 bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
 owm = pyowm.OWM(OWM_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+	start_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+	start_markup.row('/start', '/help', '/hide')
+	start_markup.row('/cheer_up')
+	start_markup.row('/weather', '/traffic', '/add_list')
+	bot.send_message(message.chat.id, "👾 The bot has started!\n⚙ Enter /help to see bot's function's")
+	bot.send_message(message.from_user.id, "⌨️ The Keyboard is added!\n⌨️ /hide To remove kb ", reply_markup=start_markup)
+    
+while True:
+	try:
+		bot.infinity_polling(True)
+	except Exception:
+		tm.sleep(1)    
