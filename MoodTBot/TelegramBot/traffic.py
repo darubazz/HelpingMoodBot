9a71eea06abba282e6a07722bc20562233d329c5
darubazz <darubazz@users.noreply.github.com>
2021-05-15 04:03:57 +0700

    Изменила URL для парсинга
    На данный момент функция работает корректно.
    Но из-за ограничения Яндекса я не могу получить доступ к данным без официального API

--- MoodTBot/TelegramBot/traffic.py
+++ MoodTBot/TelegramBot/traffic.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://yandex.ru/maps/67/tomsk/?l=trf%2Ctrfe&ll=84.988423%2C56.533638&z=10.89"
-
+URL1 = "https://yandex.ru/"
 HEADERS = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
     'accept': "*/*"}
@@ -17,8 +17,14 @@ def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
 
     message = soup.find('div', class_='traffic-panel-view__dropdown-title')
-    #traffic-panel-view__dropdown-title
     message.get_text()
+
+    #message = soup.find('div', class_='traffic__rate - text')
+    #message.get_text()
+    #traffic__rate - text
+    #home-link home-link_black_yes
+    #message = soup.find('div', class_='home-link home-link_black_yes')
+    #message.get_text()
     return message
 
 
